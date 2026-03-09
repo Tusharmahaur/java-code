5e1d2a4eb08614517229a5a0841acd0b24a83ca5
Tusharmahaur <Tusharmahaur73@gmail.com>
2026-03-09 20:53:42 +0530

    hollow pattern

--- pattern.java
+++ pattern.java
@@ -1,5 +1,22 @@
+// import java.util.*;
+// public class pattern {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter the number : ");
+//         int num = sc.nextInt();
+
+//         for(int i = 0; i<num; i++){
+//             for(int j = 0; j<num; j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
 import java.util.*;
-public class pattern {
+
+public class pattern{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number : ");
@@ -7,7 +24,13 @@ public class pattern {
 
         for(int i = 0; i<num; i++){
             for(int j = 0; j<num; j++){
-                System.out.print("*");
+                if(i==0 || i == num-1){
+                    System.out.print(" * ");
+                }else if(j == 0 || j == num-1){
+                    System.out.print(" * ");
+                }else{
+                    System.out.print("   ");
+                }
             }
             System.out.println();
         }
